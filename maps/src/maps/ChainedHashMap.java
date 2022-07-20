c53390fc0e222a08448005b9592d56f1b578e1d8
Aman Gupta <41102741+Dustygnome421@users.noreply.github.com>
2022-07-20 15:56:33 -0700

    Changed ChainedHashMap.java put method (back change)

--- maps/src/maps/ChainedHashMap.java
+++ maps/src/maps/ChainedHashMap.java
@@ -124,13 +124,13 @@ public class ChainedHashMap<K, V> extends AbstractIterableMap<K, V> {
     @Override
     public V get(Object key) {
 
-        return chains[getHasCode(key)].get(key);
-        //
-        // if (containsKey(key))
-        // {
-        //     return chains[getHasCode(key)].get(key);
-        // }
-        // return null;
+        //return chains[getHasCode(key)].get(key);
+
+        if (containsKey(key))
+        {
+            return chains[getHasCode(key)].get(key);
+        }
+        return null;
     }
 
     @Override
