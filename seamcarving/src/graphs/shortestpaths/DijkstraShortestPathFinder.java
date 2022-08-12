1fb92ff03061474c0e183877a7450d7d2c04fd85
LydiaXwQ <97652648+LydiaXwQ@users.noreply.github.com>
2022-08-12 14:45:51 -0700

    Pass 8 tests

--- seamcarving/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
+++ seamcarving/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
@@ -1,13 +1,19 @@
 package graphs.shortestpaths;
 
+import graphs.Edge;
+import graphs.EdgeWithData;
 import priorityqueues.ExtrinsicMinPQ;
 import priorityqueues.NaiveMinPQ;
 import graphs.BaseEdge;
 import graphs.Graph;
 
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Computes shortest paths using Dijkstra's algorithm.
@@ -31,16 +37,69 @@ public class DijkstraShortestPathFinder<G extends Graph<V, E>, V, E extends Base
          */
     }
 
+    private V startVertex;
+
     @Override
+    //Returns a (a map from vertex to preceding edge) containing the shortest path from start to end in given graph.
     protected Map<V, E> constructShortestPathsTree(G graph, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        ExtrinsicMinPQ<V> edges = createMinPQ();
+        Map<V, E> edgeTo = new HashMap<>();
+        Map<V, Double> distTo = new HashMap<>();
+        Set<V> known = new HashSet<>();
+        Set<V> visited = new HashSet<>();
+
+        edges.add(start, 0.0);
+        //Initiate starting vertext is start with a distance of 0
+        distTo.put(start, 0.0);
+        edgeTo.put(start, null);
+        //visited start
+        visited.add(start);
+
+        //while(!edges.isEmpty())
+        while (!edges.isEmpty()) {
+            startVertex = edges.removeMin();
+            for (E edge : graph.outgoingEdgesFrom(startVertex)) {
+                if (!visited.contains(edge.to())) {
+                    distTo.put(edge.to(), Double.POSITIVE_INFINITY);
+                    edgeTo.put(edge.to(), edge);
+                    visited.add(edge.to());
+                    edges.add(edge.to(), Double.POSITIVE_INFINITY);
+                }
+                double oldDist = distTo.get(edge.to());
+                double newDist = distTo.get(edge.from()) + edge.weight();
+                if (newDist < oldDist) {
+                    distTo.put(edge.to(), newDist);
+                    edgeTo.put(edge.to(), edge);
+                    edges.changePriority(edge.to(), newDist);
+                }
+                //edges.add(edge.to(), newDist);
+                known.add(startVertex);
+                if (known.contains(end)) {
+                    return edgeTo;
+                }
+            }
+        }
+        return edgeTo;
+
     }
 
     @Override
     protected ShortestPath<V, E> extractShortestPath(Map<V, E> spt, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (start.equals(end)) {
+            return new ShortestPath.SingleVertex<V, E>(start);
+        }
+        List<E> list = new ArrayList<>();
+        if (spt.containsKey(end)) {
+            list.add(0, spt.get(end));
+            V vertex = spt.get(end).from();
+            while (!vertex.equals(start)) {
+                list.add(spt.get(vertex));
+                vertex = spt.get(vertex).from();
+            }
+            return new ShortestPath.Success<>(list);
+        }
+        return new ShortestPath.Failure<>();
     }
 
 }
